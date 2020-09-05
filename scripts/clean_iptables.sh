da3a749f807157561d39bb0f02e95132d5decda8
AlvitrRavenclaw <943459049@qq.com>
2020-09-05 07:05:51 +0000

    add: scripts and yamls

---
+++ scripts/clean_iptables.sh
@@ -0,0 +1,15 @@
+iptables -F -t nat
+iptables -F -t raw
+iptables -F -t mangle
+iptables -F -t filter
+
+iptables -X -t nat
+iptables -X -t raw
+iptables -X -t mangle
+iptables -X -t filter
+
+iptables -Z -t nat
+iptables -Z -t raw
+iptables -Z -t mangle
+iptables -Z -t filter
+
